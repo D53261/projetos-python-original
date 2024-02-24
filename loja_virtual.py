06bf10f6511b1d9a7ba4a770a738b4cec851ec3b
Danilo Ribeiro <danilo051007@gmail.com>
2024-02-24 20:22:25 -0300

    Projetos iniciais

--- loja_virtual.py
+++ loja_virtual.py
@@ -11,6 +11,7 @@ class Produto(metaclass=ABCMeta):
     def calcular_desconto(self):
         return self.preco
 
+
     @property
     def descricao(self):
         return self._descricao
@@ -21,8 +22,17 @@ class Produto(metaclass=ABCMeta):
 
     def __str__(self):
         return f'Produto: {self._nome} / Descrição: {self.descricao} / Preço: {self.preco_atual}'
+    
+class Promocao:
+    def aplicar_promocao(self, data_atual, dia_inicio, dia_fim, desconto_promocional):
+        print(f'Há uma promoção que começou no dia {dia_inicio} e termina(ou) no dia {dia_fim}!!')
+        if data_atual >= dia_inicio and data_atual <= dia_fim:
+            print('A promoção ainda esta em vigor!!')
+            self.preco_atual -= self.preco_atual * desconto_promocional
+        else:
+            print('O prazo da promoção terminou!!')
 
-class Livro(Produto):
+class Livro(Produto, Promocao):
     def __init__(self, nome, preco, descricao, ficcao=False):
         super().__init__(nome, preco, descricao)
         self.ficcao = ficcao
@@ -34,7 +44,7 @@ class Livro(Produto):
             self.preco_atual = self.preco - (super().calcular_desconto() * 0.05)
 
 
-class Eletronico(Produto):
+class Eletronico(Produto, Promocao):
     def __init__(self, nome, preco, descricao, velho_demais=False):
         super().__init__(nome, preco, descricao)
         self.velho_demais = velho_demais
@@ -45,7 +55,7 @@ class Eletronico(Produto):
         else:
             self.preco_atual = self.preco - (super().calcular_desconto() * 0.08)
 
-class Roupas(Produto):
+class Roupas(Produto, Promocao):
     def __init__(self, nome, preco, descricao, de_verao=False):
         super().__init__(nome, preco, descricao)
         self.de_verao = de_verao
@@ -55,19 +65,6 @@ class Roupas(Produto):
             self.preco_atual = self.preco - (super().calcular_desconto() * 0.20)
         else:
             self.preco_atual = self.preco - (super().calcular_desconto() * 0.10)
-
-class Promocao:
-    def __init__(self, dia_inicio, dia_fim, desconto_promocional):
-        self.dia_inicio = dia_inicio
-        self.dia_fim = dia_fim
-        self.desconto_promocional = desconto_promocional
-
-    def descontar(self, data_atual):
-        if data_atual >= self.dia_inicio and data_atual <= self.dia_fim:
-            print('A promoção ainda esta em vigor!!')
-            self.preco_atual -= self.preco_atual * self.desconto_promocional
-        else:
-            print('O prazo da promoção terminou!!')
 
 class Lista:
     def __init__(self, nome, produtos):
@@ -79,17 +76,23 @@ class Lista:
 
     def __len__(self):
         return len(self.produtos)
+    
+
 
 livro = Livro("Harry Potter", 50.0, "Um bom livro", False)
 livro.calcular_desconto()
+livro.aplicar_promocao(3, 2, 5, 0.3)
+
 
 
 eletronico = Eletronico("SmartWach", 300.0, "Um bom aparelho", True)
 eletronico.calcular_desconto()
+eletronico.aplicar_promocao(6, 2, 12, 0.2)
 
 
 roupa = Roupas("T-shirt", 60.0, "Uma boa camiseta", True)
 roupa.calcular_desconto()
+roupa.aplicar_promocao(23, 2, 10, 0.5)
 
 
 listinha = [livro, eletronico, roupa]
